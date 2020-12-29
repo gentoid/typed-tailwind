35d7e0da1b13b37527bac0ff52639e7c92b423f4
Viktor Lazarev <taurus101v@gmail.com>
2020-12-29 07:25:59 +0100

    Add "-derive" crate with macros for ScreenSize and HoverState

---
+++ typed-tailwind-derive/src/lib.rs
@@ -0,0 +1,39 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn;
+
+#[proc_macro_derive(ScreenSize)]
+pub fn screen_size_derive(input: TokenStream) -> TokenStream {
+    let ast = syn::parse(input).unwrap();
+    impl_screen_size(&ast)
+}
+
+fn impl_screen_size(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let gen = quote! {
+        impl ScreenSizeTrait for #name {
+            fn hover(self) -> ScreenSize {
+                ScreenSize(Box::new(self))
+            }
+        }
+    };
+    gen.into()
+}
+
+#[proc_macro_derive(HoverState)]
+pub fn hover_state_derive(input: TokenStream) -> TokenStream {
+    let ast = syn::parse(input).unwrap();
+    impl_hover_state(&ast)
+}
+
+fn impl_hover_state(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let gen = quote! {
+        impl HoverStateTrait for #name {
+            fn hover(self) -> HoverState {
+                HoverState(Box::new(self))
+            }
+        }
+    };
+    gen.into()
+}
