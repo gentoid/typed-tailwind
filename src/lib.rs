dcc8209f10673c374fed8f5bd3643350d906ee9e
Viktor Lazarev <taurus101v@gmail.com>
2020-12-29 09:03:17 +0100

    Make all modes public

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-mod core;
-mod flexbox;
-mod layout;
-mod sizing;
-mod spacing;
-mod typography;
+pub mod core;
+pub mod flexbox;
+pub mod layout;
+pub mod sizing;
+pub mod spacing;
+pub mod typography;
